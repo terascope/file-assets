fca0f1e96bb7e31fadc56b5f58f66ee005757416
Zach Burnham <zburnham@terascope.io>
2018-08-07 14:57:44 -0700

    updated tests to work with async improvements and direct to file output

--- spec/csv_exporter-spec.js
+++ spec/csv_exporter-spec.js
@@ -6,6 +6,7 @@ const processor = require('../asset/csv_exporter');
 
 
 const testHarness = harness(processor);
+// console.log(testHarness.run)
 
 // Make sure there is a clean output directory for each test cycle. Removes lingering output files
 // in the directory or creates the directory if it doesn't exist
@@ -46,6 +47,12 @@ const data = [
         field3: 'even more test data',
         field2: 57
     }
+];
+
+const data2 = [
+    'record1',
+    'record2',
+    'record3'
 ];
 
 const caseMultiFileSpecifyFields = {
@@ -105,6 +112,21 @@ const caseCustomDelimiter = {
     delimiter: '\t'
 };
 
+const caseJSON2File = {
+    _op: 'csv_exporter',
+    path: './spec/test_output',
+    file_prefix: 'test',
+    d2f: true
+};
+
+const caseText2File = {
+    _op: 'csv_exporter',
+    path: './spec/test_output',
+    file_prefix: 'test',
+    d2f: true,
+    jsonIn: false
+};
+
 // const metricPayload = harness.run(data, opConfig);
 
 describe('The file-assets csv_exporter processor', () => {
@@ -118,109 +140,163 @@ describe('The file-assets csv_exporter processor', () => {
         expect(schema.include_header.default).toEqual(false);
     });
     cleanTestDir();
-    it('creates multiple files with specific fields', () => {
-        testHarness.run(data, caseMultiFileSpecifyFields);
-        testHarness.run(data, caseMultiFileSpecifyFields);
-        expect(fs.readdirSync('./spec/test_output').length).toEqual(2);
-        const file0 = fs.readFileSync('./spec/test_output/test_undefined.0', 'utf-8');
-        const file1 = fs.readFileSync('./spec/test_output/test_undefined.1', 'utf-8');
-        expect(file0).toEqual(
-            '"test data",42\n"more test data",43\n"even more test data",44\n'
-        );
-        expect(file1).toEqual(
-            '"test data",42\n"more test data",43\n"even more test data",44\n'
-        );
-        cleanTestDir();
+    it('creates multiple files with specific fields', (done) => {
+        const opConfig = caseMultiFileSpecifyFields;
+        const slices = [data, data];
+        testHarness.runSlices(slices, opConfig)
+            .then(() => {
+                // This should be equal to three since the test harnes shoves an empty slice through
+                expect(fs.readdirSync('./spec/test_output').length).toEqual(3);
+                expect(fs.readFileSync('./spec/test_output/test_undefined.0', 'utf-8')).toEqual(
+                    '"test data",42\n"more test data",43\n"even more test data",44\n'
+                );
+                expect(fs.readFileSync('./spec/test_output/test_undefined.1', 'utf-8')).toEqual(
+                    '"test data",42\n"more test data",43\n"even more test data",44\n'
+                );
+                cleanTestDir();
+                done();
+            });
     });
-    it('creates multiple files with all fields', () => {
-        testHarness.run(data, caseMultiFileAllFields);
-        testHarness.run(data, caseMultiFileAllFields);
-        expect(fs.readdirSync('./spec/test_output').length).toEqual(2);
-        const file0 = fs.readFileSync('./spec/test_output/test_undefined.0', 'utf-8');
-        const file1 = fs.readFileSync('./spec/test_output/test_undefined.1', 'utf-8');
-        expect(file0).toEqual(
-            '42,"test data",55\n43,"more test data",56\n44,"even more test data",57\n'
-        );
-        expect(file1).toEqual(
-            '42,"test data",55\n43,"more test data",56\n44,"even more test data",57\n'
-        );
-        cleanTestDir();
+    it('creates multiple files with all fields', (done) => {
+        const opConfig = caseMultiFileAllFields;
+        const slices = [data, data];
+        testHarness.runSlices(slices, opConfig)
+            .then(() => {
+                // This should be equal to three since the test harnes shoves an empty slice through
+                expect(fs.readdirSync('./spec/test_output').length).toEqual(3);
+                expect(fs.readFileSync('./spec/test_output/test_undefined.0', 'utf-8')).toEqual(
+                    '42,"test data",55\n43,"more test data",56\n44,"even more test data",57\n'
+                );
+                expect(fs.readFileSync('./spec/test_output/test_undefined.1', 'utf-8')).toEqual(
+                    '42,"test data",55\n43,"more test data",56\n44,"even more test data",57\n'
+                );
+                cleanTestDir();
+                done();
+            });
     });
-    it('creates multiple files with all fields and headers', () => {
-        testHarness.run(data, caseMultiFileAllFieldsHeader);
-        testHarness.run(data, caseMultiFileAllFieldsHeader);
-        expect(fs.readdirSync('./spec/test_output').length).toEqual(2);
-        const file0 = fs.readFileSync('./spec/test_output/test_undefined.0', 'utf-8');
-        const file1 = fs.readFileSync('./spec/test_output/test_undefined.1', 'utf-8');
-        expect(file0).toEqual(
-            '"field1","field3","field2"\n'
-            + '42,"test data",55\n'
-            + '43,"more test data",56\n'
-            + '44,"even more test data",57\n'
-        );
-        expect(file1).toEqual(
-            '"field1","field3","field2"\n'
-            + '42,"test data",55\n'
-            + '43,"more test data",56\n'
-            + '44,"even more test data",57\n'
-        );
-        cleanTestDir();
+    it('creates multiple files with all fields and headers', (done) => {
+        const opConfig = caseMultiFileAllFieldsHeader;
+        const slices = [data, data];
+        testHarness.runSlices(slices, opConfig)
+            .then(() => {
+                // This should be equal to three since the test harnes shoves an empty slice through
+                expect(fs.readdirSync('./spec/test_output').length).toEqual(3);
+                expect(fs.readFileSync('./spec/test_output/test_undefined.0', 'utf-8')).toEqual(
+                    '"field1","field3","field2"\n'
+                    + '42,"test data",55\n'
+                    + '43,"more test data",56\n'
+                    + '44,"even more test data",57\n'
+                );
+                expect(fs.readFileSync('./spec/test_output/test_undefined.1', 'utf-8')).toEqual(
+                    '"field1","field3","field2"\n'
+                    + '42,"test data",55\n'
+                    + '43,"more test data",56\n'
+                    + '44,"even more test data",57\n'
+                );
+                cleanTestDir();
+                done();
+            });
     });
-    it('creates a single file with custom fields', () => {
-        testHarness.run(data, caseSingleFileSpecifyFields);
-        testHarness.run(data, caseSingleFileSpecifyFields);
-        expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
-        const file = fs.readFileSync('./spec/test_output/test_undefined', 'utf-8');
-        expect(file).toEqual(
-            '"test data",42\n'
-            + '"more test data",43\n'
-            + '"even more test data",44\n'
-            + '"test data",42\n'
-            + '"more test data",43\n'
-            + '"even more test data",44\n'
-        );
-        cleanTestDir();
+    it('creates a single file with custom fields', (done) => {
+        const opConfig = caseSingleFileSpecifyFields;
+        const slices = [data, data];
+        testHarness.runSlices(slices, opConfig)
+            .then(() => {
+                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
+                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                    '"test data",42\n'
+                    + '"more test data",43\n'
+                    + '"even more test data",44\n'
+                    + '"test data",42\n'
+                    + '"more test data",43\n'
+                    + '"even more test data",44\n\n'
+                );
+                cleanTestDir();
+                done();
+            });
     });
-    it('creates a single file with all fields', () => {
-        testHarness.run(data, caseSingleFileAllFields);
-        testHarness.run(data, caseSingleFileAllFields);
-        expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
-        const file = fs.readFileSync('./spec/test_output/test_undefined', 'utf-8');
-        expect(file).toEqual(
-            '42,"test data",55\n'
-            + '43,"more test data",56\n'
-            + '44,"even more test data",57\n'
-            + '42,"test data",55\n'
-            + '43,"more test data",56\n'
-            + '44,"even more test data",57\n'
-        );
-        cleanTestDir();
+    it('creates a single file with all fields', (done) => {
+        const opConfig = caseSingleFileAllFields;
+        const slices = [data, data];
+        testHarness.runSlices(slices, opConfig)
+            .then(() => {
+                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
+                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                    '42,"test data",55\n'
+                    + '43,"more test data",56\n'
+                    + '44,"even more test data",57\n'
+                    + '42,"test data",55\n'
+                    + '43,"more test data",56\n'
+                    + '44,"even more test data",57\n\n'
+                );
+                cleanTestDir();
+                done();
+            });
     });
-    it('creates a single file and adds a header properly', () => {
-        testHarness.run(data, caseSingleFileAllFieldsHeader);
-        testHarness.run(data, caseSingleFileAllFieldsHeader);
-        expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
-        const file = fs.readFileSync('./spec/test_output/test_undefined', 'utf-8');
-        expect(file).toEqual(
-            '"field1","field3","field2"\n'
-            + '42,"test data",55\n'
-            + '43,"more test data",56\n'
-            + '44,"even more test data",57\n'
-            + '42,"test data",55\n'
-            + '43,"more test data",56\n'
-            + '44,"even more test data",57\n'
-        );
-        cleanTestDir();
+    it('creates a single file and adds a header properly', (done) => {
+        const opConfig = caseSingleFileAllFieldsHeader;
+        const slices = [data, data];
+        testHarness.runSlices(slices, opConfig)
+            .then(() => {
+                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
+                // The empty slice does leave an extra newline at the end of the file, but for some
+                // reason this check will fail if that is included in the check
+                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                    '"field1","field3","field2"\n'
+                    + '42,"test data",55\n'
+                    + '43,"more test data",56\n'
+                    + '44,"even more test data",57\n'
+                    + '42,"test data",55\n'
+                    + '43,"more test data",56\n'
+                    + '44,"even more test data",57\n\n'
+                );
+                cleanTestDir();
+                done();
+            });
     });
-    it('creates a single file with a custom delimiter', () => {
-        testHarness.run(data, caseCustomDelimiter);
-        expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
-        const file = fs.readFileSync('./spec/test_output/test_undefined', 'utf-8');
-        expect(file).toEqual(
-            '42\t"test data"\t55\n'
-            + '43\t"more test data"\t56\n'
-            + '44\t"even more test data"\t57\n'
-        );
-        cleanTestDir();
+    it('creates a single file with a custom delimiter', (done) => {
+        const opConfig = caseCustomDelimiter;
+        const slices = [data];
+        testHarness.runSlices(slices, opConfig)
+            .then(() => {
+                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
+                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                    '42\t"test data"\t55\n'
+                    + '43\t"more test data"\t56\n'
+                    + '44\t"even more test data"\t57\n\n'
+                );
+                cleanTestDir();
+                done();
+            });
+    });
+    it('creates a single file with JSON records on each line', (done) => {
+        const opConfig = caseJSON2File;
+        const slices = [data];
+        testHarness.runSlices(slices, opConfig)
+            .then(() => {
+                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
+                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                    '{"field1":42,"field3":"test data","field2":55}\n'
+                    + '{"field1":43,"field3":"more test data","field2":56}\n'
+                    + '{"field1":44,"field3":"even more test data","field2":57}\n'
+                );
+                cleanTestDir();
+                done();
+            });
+    });
+    it('creates a single file with text records on each line', (done) => {
+        const opConfig = caseText2File;
+        const slices = [data2];
+        testHarness.runSlices(slices, opConfig)
+            .then(() => {
+                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
+                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                    'record1\n'
+                    + 'record2\n'
+                    + 'record3\n'
+                );
+                cleanTestDir();
+                done();
+            });
     });
 });
