80463bd4c5c588836473a323c8ef0ef4f52a29ef
Erik Stephens <estephens@terascope.io>
2018-09-19 15:32:24 -0700

    Get file_exporter tests passing again

--- test/file_exporter-spec.js
+++ test/file_exporter-spec.js
@@ -11,12 +11,12 @@ const testHarness = harness(processor);
 // Make sure there is a clean output directory for each test cycle. Removes lingering output files
 // in the directory or creates the directory if it doesn't exist
 function cleanTestDir() {
-    if (fs.readdirSync('./spec').indexOf('test_output') > -1) {
-        if (fs.readdirSync('./spec/test_output').length > 0) {
-            fs.readdirSync('./spec/test_output').forEach(file => fs.unlinkSync(`./spec/test_output/${file}`));
+    if (fs.readdirSync('./test').indexOf('test_output') > -1) {
+        if (fs.readdirSync('./test/test_output').length > 0) {
+            fs.readdirSync('./test/test_output').forEach(file => fs.unlinkSync(`./test/test_output/${file}`));
         }
     } else {
-        fs.mkdirSync('./spec/test_output');
+        fs.mkdirSync('./test/test_output');
     }
 }
 
@@ -57,7 +57,7 @@ const data2 = [
 
 const caseMultiFileSpecifyFields = {
     _op: 'csv_exporter',
-    path: './spec/test_output',
+    path: './test/test_output',
     file_prefix: 'test',
     format: 'csv',
     fields: [
@@ -69,7 +69,7 @@ const caseMultiFileSpecifyFields = {
 
 const caseMultiFileAllFields = {
     _op: 'csv_exporter',
-    path: './spec/test_output',
+    path: './test/test_output',
     file_prefix: 'test',
     format: 'csv',
     file_per_slice: true
@@ -77,7 +77,7 @@ const caseMultiFileAllFields = {
 
 const caseMultiFileAllFieldsHeader = {
     _op: 'csv_exporter',
-    path: './spec/test_output',
+    path: './test/test_output',
     file_prefix: 'test',
     file_per_slice: true,
     format: 'csv',
@@ -86,7 +86,7 @@ const caseMultiFileAllFieldsHeader = {
 
 const caseSingleFileSpecifyFields = {
     _op: 'csv_exporter',
-    path: './spec/test_output',
+    path: './test/test_output',
     file_prefix: 'test',
     format: 'csv',
     fields: [
@@ -97,14 +97,14 @@ const caseSingleFileSpecifyFields = {
 
 const caseSingleFileAllFields = {
     _op: 'csv_exporter',
-    path: './spec/test_output',
+    path: './test/test_output',
     format: 'csv',
     file_prefix: 'test'
 };
 
 const caseSingleFileAllFieldsHeader = {
     _op: 'csv_exporter',
-    path: './spec/test_output',
+    path: './test/test_output',
     file_prefix: 'test',
     format: 'csv',
     include_header: true
@@ -113,7 +113,7 @@ const caseSingleFileAllFieldsHeader = {
 // Testing a tab delimiter
 const caseTabDelimiter = {
     _op: 'csv_exporter',
-    path: './spec/test_output',
+    path: './test/test_output',
     file_prefix: 'test',
     format: 'tsv'
 };
@@ -121,7 +121,7 @@ const caseTabDelimiter = {
 // Testing a custom delimiter
 const caseCustomDelimiter = {
     _op: 'csv_exporter',
-    path: './spec/test_output',
+    path: './test/test_output',
     file_prefix: 'test',
     delimiter: '^',
     format: 'csv'
@@ -129,14 +129,14 @@ const caseCustomDelimiter = {
 
 const caseJSON2File = {
     _op: 'csv_exporter',
-    path: './spec/test_output',
+    path: './test/test_output',
     file_prefix: 'test',
     format: 'json'
 };
 
 const caseText2File = {
     _op: 'csv_exporter',
-    path: './spec/test_output',
+    path: './test/test_output',
     file_prefix: 'test',
     format: 'text'
 };
@@ -161,11 +161,11 @@ describe('The file-assets csv_exporter processor', () => {
         testHarness.runSlices(slices, opConfig)
             .then(() => {
                 // This should be equal to three since the test harnes shoves an empty slice through
-                expect(fs.readdirSync('./spec/test_output').length).toEqual(3);
-                expect(fs.readFileSync('./spec/test_output/test_undefined.0', 'utf-8')).toEqual(
+                expect(fs.readdirSync('./test/test_output').length).toEqual(3);
+                expect(fs.readFileSync('./test/test_output/test_undefined.0', 'utf-8')).toEqual(
                     '"test data",42\n"more test data",43\n"even more test data",44\n'
                 );
-                expect(fs.readFileSync('./spec/test_output/test_undefined.1', 'utf-8')).toEqual(
+                expect(fs.readFileSync('./test/test_output/test_undefined.1', 'utf-8')).toEqual(
                     '"test data",42\n"more test data",43\n"even more test data",44\n'
                 );
                 cleanTestDir();
@@ -178,11 +178,11 @@ describe('The file-assets csv_exporter processor', () => {
         testHarness.runSlices(slices, opConfig)
             .then(() => {
                 // This should be equal to three since the test harnes shoves an empty slice through
-                expect(fs.readdirSync('./spec/test_output').length).toEqual(3);
-                expect(fs.readFileSync('./spec/test_output/test_undefined.0', 'utf-8')).toEqual(
+                expect(fs.readdirSync('./test/test_output').length).toEqual(3);
+                expect(fs.readFileSync('./test/test_output/test_undefined.0', 'utf-8')).toEqual(
                     '42,"test data",55\n43,"more test data",56\n44,"even more test data",57\n'
                 );
-                expect(fs.readFileSync('./spec/test_output/test_undefined.1', 'utf-8')).toEqual(
+                expect(fs.readFileSync('./test/test_output/test_undefined.1', 'utf-8')).toEqual(
                     '42,"test data",55\n43,"more test data",56\n44,"even more test data",57\n'
                 );
                 cleanTestDir();
@@ -195,14 +195,14 @@ describe('The file-assets csv_exporter processor', () => {
         testHarness.runSlices(slices, opConfig)
             .then(() => {
                 // This should be equal to three since the test harnes shoves an empty slice through
-                expect(fs.readdirSync('./spec/test_output').length).toEqual(3);
-                expect(fs.readFileSync('./spec/test_output/test_undefined.0', 'utf-8')).toEqual(
+                expect(fs.readdirSync('./test/test_output').length).toEqual(3);
+                expect(fs.readFileSync('./test/test_output/test_undefined.0', 'utf-8')).toEqual(
                     '"field1","field3","field2"\n'
                     + '42,"test data",55\n'
                     + '43,"more test data",56\n'
                     + '44,"even more test data",57\n'
                 );
-                expect(fs.readFileSync('./spec/test_output/test_undefined.1', 'utf-8')).toEqual(
+                expect(fs.readFileSync('./test/test_output/test_undefined.1', 'utf-8')).toEqual(
                     '"field1","field3","field2"\n'
                     + '42,"test data",55\n'
                     + '43,"more test data",56\n'
@@ -217,8 +217,8 @@ describe('The file-assets csv_exporter processor', () => {
         const slices = [data, data];
         testHarness.runSlices(slices, opConfig)
             .then(() => {
-                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
-                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                expect(fs.readdirSync('./test/test_output').length).toEqual(1);
+                expect(fs.readFileSync('./test/test_output/test_undefined', 'utf-8')).toEqual(
                     '"test data",42\n'
                     + '"more test data",43\n'
                     + '"even more test data",44\n'
@@ -235,8 +235,8 @@ describe('The file-assets csv_exporter processor', () => {
         const slices = [data, data];
         testHarness.runSlices(slices, opConfig)
             .then(() => {
-                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
-                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                expect(fs.readdirSync('./test/test_output').length).toEqual(1);
+                expect(fs.readFileSync('./test/test_output/test_undefined', 'utf-8')).toEqual(
                     '42,"test data",55\n'
                     + '43,"more test data",56\n'
                     + '44,"even more test data",57\n'
@@ -253,10 +253,10 @@ describe('The file-assets csv_exporter processor', () => {
         const slices = [data, data];
         testHarness.runSlices(slices, opConfig)
             .then(() => {
-                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
+                expect(fs.readdirSync('./test/test_output').length).toEqual(1);
                 // The empty slice does leave an extra newline at the end of the file, but for some
                 // reason this check will fail if that is included in the check
-                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                expect(fs.readFileSync('./test/test_output/test_undefined', 'utf-8')).toEqual(
                     '"field1","field3","field2"\n'
                     + '42,"test data",55\n'
                     + '43,"more test data",56\n'
@@ -274,8 +274,8 @@ describe('The file-assets csv_exporter processor', () => {
         const slices = [data];
         testHarness.runSlices(slices, opConfig)
             .then(() => {
-                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
-                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                expect(fs.readdirSync('./test/test_output').length).toEqual(1);
+                expect(fs.readFileSync('./test/test_output/test_undefined', 'utf-8')).toEqual(
                     '42\t"test data"\t55\n'
                     + '43\t"more test data"\t56\n'
                     + '44\t"even more test data"\t57\n\n'
@@ -289,8 +289,8 @@ describe('The file-assets csv_exporter processor', () => {
         const slices = [data];
         testHarness.runSlices(slices, opConfig)
             .then(() => {
-                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
-                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                expect(fs.readdirSync('./test/test_output').length).toEqual(1);
+                expect(fs.readFileSync('./test/test_output/test_undefined', 'utf-8')).toEqual(
                     '42^"test data"^55\n'
                     + '43^"more test data"^56\n'
                     + '44^"even more test data"^57\n\n'
@@ -304,8 +304,8 @@ describe('The file-assets csv_exporter processor', () => {
         const slices = [data];
         testHarness.runSlices(slices, opConfig)
             .then(() => {
-                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
-                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                expect(fs.readdirSync('./test/test_output').length).toEqual(1);
+                expect(fs.readFileSync('./test/test_output/test_undefined', 'utf-8')).toEqual(
                     '{"field1":42,"field3":"test data","field2":55}\n'
                     + '{"field1":43,"field3":"more test data","field2":56}\n'
                     + '{"field1":44,"field3":"even more test data","field2":57}\n'
@@ -319,8 +319,8 @@ describe('The file-assets csv_exporter processor', () => {
         const slices = [data2];
         testHarness.runSlices(slices, opConfig)
             .then(() => {
-                expect(fs.readdirSync('./spec/test_output').length).toEqual(1);
-                expect(fs.readFileSync('./spec/test_output/test_undefined', 'utf-8')).toEqual(
+                expect(fs.readdirSync('./test/test_output').length).toEqual(1);
+                expect(fs.readFileSync('./test/test_output/test_undefined', 'utf-8')).toEqual(
                     'record1\n'
                     + 'record2\n'
                     + 'record3\n'
