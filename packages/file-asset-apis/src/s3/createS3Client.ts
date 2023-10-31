1a69308e360b8661b506cac54573fecf4174e9ab
Lesley Dreyer <lesley@terascope.io>
2023-10-30 17:14:07 -0700

    adjust create client for types & to use the httpOpts even if not ssl

--- packages/file-asset-apis/src/s3/createS3Client.ts
+++ packages/file-asset-apis/src/s3/createS3Client.ts
@@ -1,15 +1,18 @@
 import fs from 'fs-extra';
-import { Agent } from 'https';
+import { Agent, AgentOptions as HttpsAgentOptions } from 'https';
 import { S3Client as BaseClient } from '@aws-sdk/client-s3';
-import { NodeHttpHandler } from '@aws-sdk/node-http-handler';
+import { NodeHttpHandler, NodeHttpHandlerOptions } from '@aws-sdk/node-http-handler';
 import type { S3ClientConfig as baseConfig } from '@aws-sdk/client-s3';
-import { debugLogger, has } from '@terascope/utils';
+import {
+    debugLogger, has, isEmpty, isNumber
+} from '@terascope/utils';
 import type { S3Client } from './client-types';
 
 export interface S3ClientConfig extends baseConfig {
     sslEnabled?: boolean,
     certLocation?: string,
-    httpOptions?: object,
+    httpOptions?: HttpsAgentOptions,
+    handlerOptions?: Pick<NodeHttpHandlerOptions, 'requestTimeout'|'connectionTimeout'>,
     secretAccessKey?: string,
     accessKeyId?: string
 }
@@ -20,12 +23,16 @@ export async function createS3Client(
 ): Promise<S3Client> {
     logger.info(`Using S3 endpoint: ${config.endpoint}`);
 
-    // The aws v3 client logs every request and its metadata, it is to intrusive
-    // should only be used in trace mode otherwise it will log the body request
+    // The aws v3 client logs every request and its metadata, it is too intrusive
+    // so should only be used in trace mode otherwise it will log the body request
     // which has heavy performance implications
     if (logger.level() === 10) {
         config.logger = logger;
     }
+
+    let httpOptions = Object.assign(
+        config.httpOptions || {}
+    );
 
     // pull certLocation from env
     // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/node-registering-certs.html
@@ -42,26 +49,31 @@ export async function createS3Client(
         // Assumes all certs needed are in a single bundle
         const certs = await fs.readFile(certPath);
 
-        const configHttpOptions = config.httpOptions ?? {};
+        httpOptions = {
+            ...httpOptions,
+            rejectUnauthorized: true,
+            ca: [certs]
+        };
+    }
 
-        const httpOptions = Object.assign(
-            { rejectUnauthorized: true },
-            configHttpOptions,
-            { ca: [certs] }
-        );
+    const { connectionTimeout, requestTimeout } = config.handlerOptions || {};
+    const requestHandlerOptions = {
+        ...isNumber(connectionTimeout) && { connectionTimeout },
+        ...isNumber(requestTimeout) && { requestTimeout },
+        ...!isEmpty(httpOptions) && new Agent(httpOptions)
+    };
 
-        config.requestHandler = new NodeHttpHandler({
-            httpsAgent: new Agent(httpOptions)
-        });
+    if (!isEmpty(requestHandlerOptions)) {
+        config.requestHandler = new NodeHttpHandler(requestHandlerOptions);
     }
 
     // config specified old style, need to move top level values into credentials
     if (!has(config, 'credentials') && has(config, 'accessKeyId') && has(config, 'secretAccessKey')) {
-        const { accessKeyId, secretAccessKey } = config;
+        const { accessKeyId = '', secretAccessKey = '' } = config;
         config.credentials = {
             accessKeyId,
             secretAccessKey
-        } as any;
+        };
     }
 
     return new BaseClient(config);
