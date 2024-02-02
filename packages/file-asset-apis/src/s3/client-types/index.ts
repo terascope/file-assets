6492ca580eb83cec2a152b658e650c844b126c64
Charlie Iorg <ciorg@terascope.io>
2024-02-02 16:12:24 -0700

    changed retry type file name

--- packages/file-asset-apis/src/s3/client-types/index.ts
+++ packages/file-asset-apis/src/s3/client-types/index.ts
@@ -1,4 +1,4 @@
 export type { S3Client } from '@aws-sdk/client-s3';
 export * as S3ClientParams from './client-params';
 export * as S3ClientResponse from './client-response';
-export * as S3RetryRequest from './retry-params';
+export * as S3RetryRequest from './request-with-retry';
