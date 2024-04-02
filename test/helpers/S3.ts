964debd59aeab340b31c329627515db11874462a
busma13 <pluitjens@terascope.io>
2024-04-02 13:38:36 -0700

    revert S3.ts test

--- test/helpers/S3.ts
+++ test/helpers/S3.ts
@@ -15,9 +15,11 @@ const logger = debugLogger('s3_tests');
 export async function makeClient() {
     return createS3Client({
         endpoint: s3Config.MINIO_HOST,
-        accessKeyId: s3Config.MINIO_ACCESS_KEY,
-        secretAccessKey: s3Config.MINIO_SECRET_KEY,
-        maxRetries: 4,
+        credentials: {
+            accessKeyId: s3Config.MINIO_ACCESS_KEY,
+            secretAccessKey: s3Config.MINIO_SECRET_KEY,
+        },
+        maxAttempts: 4,
         forcePathStyle: true,
         sslEnabled: false,
         region: 'us-east-1'
