b30370b6697ad7ae790375ee788ae5e7a1f73b1e
sotojn <jsoto@terascope.com>
2024-05-20 09:20:19 -0700

    add records_read_from_s3 metric in S3_reader

--- asset/src/s3_reader/fetcher.ts
+++ asset/src/s3_reader/fetcher.ts
@@ -5,15 +5,36 @@ import { S3ReaderFactoryAPI } from '../s3_reader_api/interfaces';
 
 export default class S3Fetcher extends Fetcher<S3ReaderConfig> {
     api!: S3TerasliceAPI;
+    private s3RecordsRead = 0;
 
     async initialize(): Promise<void> {
         await super.initialize();
         const apiName = this.opConfig.api_name;
         const apiManager = this.getAPI<S3ReaderFactoryAPI>(apiName);
         this.api = await apiManager.create(apiName, {} as any);
+        const { context } = this;
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
+        const self = this;
+        await this.context.apis.foundation.promMetrics.addGauge(
+            'records_read_from_s3',
+            'Number of records read from s3',
+            ['class'],
+            async function collect() {
+                const labels = {
+                    class: 'S3Fetcher',
+                    ...context.apis.foundation.promMetrics.getDefaultLabels()
+                };
+                this.set(labels, self.getTotalReadS3Records());
+            });
     }
 
     async fetch(slice: FileSlice): Promise<DataEntity[]> {
-        return this.api.read(slice);
+        const data = await this.api.read(slice);
+        this.s3RecordsRead += data.length;
+        return data;
+    }
+
+    getTotalReadS3Records(): number {
+        return this.s3RecordsRead;
     }
 }
