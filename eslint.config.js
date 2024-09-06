554a170e61ce1ba0a1c1b3b3061344658bc50b50
Jared Noble <jsnoble1@gmail.com>
2024-09-06 09:26:59 -0700

    fix lint call and ignore dist folder

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
 import eslintConfig from '@terascope/eslint-config';
+// need to probably put this in original eslint-config
+// don't lint the dist folder
+eslintConfig[0].ignores.push('dist/');
 
 export default eslintConfig
