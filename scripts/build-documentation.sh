6c9bf0df8f3822ad2cb48a9a46538110333971d9
sotojn <jsoto@terascope.com>
2026-03-05 14:39:21 -0700

    update docs build file to use pnpm

--- scripts/build-documentation.sh
+++ scripts/build-documentation.sh
@@ -25,12 +25,12 @@ main() {
         ;;
     esac
 
-    yarn setup &&
-        yarn docs &&
+    pnpm run setup &&
+        pnpm docs &&
         git add -f docs/packages/*/api &&
         cd website &&
-        yarn install  &&
-        yarn run build &&
+        pnpm install  &&
+        pnpm run build &&
         git restore --staged ../docs/packages/*/api
 }
 
