a668804244c6ed7389b47e43eb52dd2de410cd6c
busma13 <pluitjens@terascope.io>
2026-03-23 14:47:56 -0700

    add encrypted tests

--- test/helpers/config.ts
+++ test/helpers/config.ts
@@ -1,7 +1,12 @@
 const {
+    ENCRYPT_MINIO = 'false',
     MINIO_HOST = 'http://127.0.0.1:9000',
     MINIO_ACCESS_KEY = 'minioadmin',
     MINIO_SECRET_KEY = 'minioadmin',
+    CERT_PATH = '',
 } = process.env;
 
-export { MINIO_HOST, MINIO_ACCESS_KEY, MINIO_SECRET_KEY };
+export {
+    ENCRYPT_MINIO, MINIO_HOST, MINIO_ACCESS_KEY,
+    MINIO_SECRET_KEY, CERT_PATH
+};
