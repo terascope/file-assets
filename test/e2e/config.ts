672bd39b1e47497deb65249f5540798832d48510
Joseph Soto <jsoto@terascope.com>
2026-03-31 10:42:21 -0700

    [tests, CI] Add e2e tests that use teraslice  (#1397)
    This PR makes the following changes:
    
    - Adds `teraslice-client-js` as a devDependency 
    - Adds simple `s3_reader` and `s3_exporter` e2e test job 
    - This these tests will run against the latest teraslice image published
    to ghcr

---
+++ test/e2e/config.ts
@@ -0,0 +1,27 @@
+import { SchemaValidator } from '@terascope/core-utils';
+import { Terafoundation } from '@terascope/types';
+
+type E2EConfig = {
+    TERASLICE_HOST: string;
+    ASSET_ZIP_PATH: string;
+    TEST_TERASLICE: boolean;
+};
+
+const schema: Terafoundation.Schema<any> = {
+    TERASLICE_HOST: {
+        default: null,
+        format: 'required_string'
+    },
+    ASSET_ZIP_PATH: {
+        default: null,
+        format: 'required_string'
+    },
+    TEST_TERASLICE: {
+        default: false,
+        format: Boolean,
+    },
+};
+
+const validator = new SchemaValidator<E2EConfig>(schema, 'e2e-config', undefined, 'allow');
+
+export default validator.validate(process.env);
